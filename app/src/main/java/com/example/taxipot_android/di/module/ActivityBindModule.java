7e9a4c52a3229caad1c8ef79dfc151962d496beb
nwar-Jeon <nwar0825@naver.com>
2019-09-08 16:46:46 +0900

    회원가입 화면 작업 완료.

--- app/src/main/java/com/example/taxipot_android/di/module/ActivityBindModule.java
+++ app/src/main/java/com/example/taxipot_android/di/module/ActivityBindModule.java
@@ -1,8 +1,12 @@
 package com.example.taxipot_android.di.module;
 
 import com.example.taxipot_android.di.ActivityScope;
-import com.example.taxipot_android.di.module.SignIn.SignInActivityModule;
+import com.example.taxipot_android.di.module.activity.SignInActivityModule;
+import com.example.taxipot_android.di.module.activity.SignUp2ActivityModule;
+import com.example.taxipot_android.di.module.activity.SignUpActivityModule;
 import com.example.taxipot_android.presenter.ui.activity.SignInActivity;
+import com.example.taxipot_android.presenter.ui.activity.SignUp2Activity;
+import com.example.taxipot_android.presenter.ui.activity.SignUpActivity;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
@@ -12,4 +16,12 @@ public abstract class ActivityBindModule {
     @ActivityScope
     @ContributesAndroidInjector(modules = SignInActivityModule.class)
     abstract SignInActivity provideSignInActivity();
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = SignUpActivityModule.class)
+    abstract SignUpActivity provideSignUpActivity();
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = SignUp2ActivityModule.class)
+    abstract SignUp2Activity provideSignUp2Activity();
 }
--- app/src/main/java/com/example/taxipot_android/presenter/ui/BaseActivity.java
+++ app/src/main/java/com/example/taxipot_android/presenter/ui/BaseActivity.java
@@ -3,5 +3,5 @@ package com.example.taxipot_android.presenter.ui;
 import dagger.android.DaggerActivity;
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class BaseActivity extends DaggerActivity {
+public class BaseActivity extends DaggerAppCompatActivity {
 }
