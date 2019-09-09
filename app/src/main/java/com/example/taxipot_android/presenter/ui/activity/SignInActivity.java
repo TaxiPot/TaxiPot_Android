41d829d192c8e585cf97fca4df439291fdb513ed
nwar-Jeon <nwar0825@naver.com>
2019-09-09 10:26:59 +0900

    Update SignInActivity.java

--- app/src/main/java/com/example/taxipot_android/presenter/ui/activity/SignInActivity.java
+++ app/src/main/java/com/example/taxipot_android/presenter/ui/activity/SignInActivity.java
@@ -2,7 +2,6 @@ package com.example.taxipot_android.presenter.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.databinding.DataBindingUtil;
@@ -11,23 +10,22 @@ import com.example.taxipot_android.R;
 import com.example.taxipot_android.databinding.ActivitySignInBinding;
 import com.example.taxipot_android.presenter.ui.BaseActivity;
 
-import javax.inject.Inject;
-
 public class SignInActivity extends BaseActivity {
 
     ActivitySignInBinding binding;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_in);
         binding.setActivity(this);
     }
-        public void doSignUp(View v) {
-            startActivity(new Intent(this, SignUpActivity.class));    
-            }
-        public void doSignIn(View v) {
+
+    public void doSignUp(View v) {
+        startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+    }
+
+    public void doSignIn(View v) {
         startActivity(new Intent(SignInActivity.this, MainActivity.class));
     }
-    
 }
