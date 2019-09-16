f1300d14928e399a213bf5e6e4a44bdb21c137f1
nwar-Jeon <nwar0825@naver.com>
2019-09-16 14:59:58 +0900

    이미지 수정,(커스텀 벡터)

---
+++ app/src/main/java/com/example/taxipot_android/presenter/ui/activity/ReportActivity.java
@@ -0,0 +1,17 @@
+package com.example.taxipot_android.presenter.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.taxipot_android.R;
+import com.example.taxipot_android.presenter.ui.BaseActivity;
+
+public class ReportActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
