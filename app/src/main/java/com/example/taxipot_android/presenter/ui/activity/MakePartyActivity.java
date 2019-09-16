c535d7c9ce9f74b08fa0b2c0e31dba5e70d90c6a
MKGOLD <dkqk0124@naver.com>
2019-09-16 14:49:37 +0900

    [Add] MakePartyActivity 제작, HomeFragment 와 연동중 프로젝트 업데이트 위한 커밋.

---
+++ app/src/main/java/com/example/taxipot_android/presenter/ui/activity/MakePartyActivity.java
@@ -0,0 +1,14 @@
+package com.example.taxipot_android.presenter.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.taxipot_android.R;
+
+public class MakePartyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_party);
+    }
+}
