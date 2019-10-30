93f89f30c42b5e0e1aafa3765e4f038daff9464d
MKGOLD <dkqk0124@naver.com>
2019-10-30 15:27:56 +0900

    [Add] FindActivity, SelectDepartureFragment 추가

---
+++ app/src/main/java/com/example/taxipot_android/presenter/ui/activity/FindPartyActivity.java
@@ -0,0 +1,21 @@
+package com.example.taxipot_android.presenter.ui.activity;
+
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.taxipot_android.R;
+import com.example.taxipot_android.databinding.ActivityFindPartyBinding;
+import com.example.taxipot_android.presenter.ui.BaseActivity;
+
+public class FindPartyActivity extends BaseActivity {
+
+    ActivityFindPartyBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_find_party);
+        binding.setActivity(this);
+    }
+}
