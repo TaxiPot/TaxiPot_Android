f2abe244c16fd12c4598e2db44b34e26882a3b6d
MKGOLD <dkqk0124@naver.com>
2019-11-19 16:17:29 +0900

    [Add] TedPermission Rx2 version을 이용하여 위치 권한 받아오는 로직 작성

--- app/src/main/java/com/example/taxipot_android/presenter/ui/fragment/SelectDepartureFragment.java
+++ app/src/main/java/com/example/taxipot_android/presenter/ui/fragment/SelectDepartureFragment.java
@@ -1,5 +1,6 @@
 package com.example.taxipot_android.presenter.ui.fragment;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
@@ -12,14 +13,67 @@ import androidx.annotation.Nullable;
 import com.example.taxipot_android.R;
 import com.example.taxipot_android.databinding.FragmentSelectDepartureBinding;
 import com.example.taxipot_android.presenter.ui.BaseFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.tedpark.tedpermission.rx2.TedRx2Permission;
 
-public class SelectDepartureFragment extends BaseFragment<FragmentSelectDepartureBinding> {
+public class SelectDepartureFragment extends BaseFragment<FragmentSelectDepartureBinding>
+        implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleMap map;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setFragmentLayout(R.layout.fragment_select_departure);
         View v = super.onCreateView(inflater, container, savedInstanceState);
         binding.setFragment(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.mapview);
+        mapFragment.getMapAsync(this);
+
+        getLocationPermission();
+
         return v;
+    }
+
+    public void getLocationPermission() {
+        TedRx2Permission.with(getActivity().getApplicationContext())
+                .setRationaleTitle("권한 요청")
+                .setRationaleMessage("위치 권한이 필요합니다. 승인하시겠습니까?")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .request()
+                .subscribe(tedPermissionResult -> {
+                    if (tedPermissionResult.isGranted()) {
+                        makeToast("위치 권한 요청 성공");
+                    } else {
+                        makeToast("위치 권한 요청 실패, " + tedPermissionResult.getDeniedPermissions().toString());
+                    }
+                }, throwable -> {
+                });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
