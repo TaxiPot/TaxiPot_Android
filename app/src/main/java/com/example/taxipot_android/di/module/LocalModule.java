477ade03a85ec1db45257ce2b5c114662d39aba9
nwar-Jeon <nwar0825@naver.com>
2019-11-25 23:15:51 +0900

    add Singleton.

--- app/src/main/java/com/example/taxipot_android/di/module/LocalModule.java
+++ app/src/main/java/com/example/taxipot_android/di/module/LocalModule.java
@@ -3,12 +3,15 @@ package com.example.taxipot_android.di.module;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class LocalModule {
     @Provides
+    @Singleton
     public SharedPreferences sharedPreferences(Context context) {
         return context.getSharedPreferences("preference",Context.MODE_PRIVATE);
     }
